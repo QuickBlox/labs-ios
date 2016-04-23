ee12663c7547cd397eccea8110ec582394ab2ccb
Andrey Ivanov <this.andrey@gmail.com>
2016-04-23 20:25:31 +0300

    update code style for Message view component + remove unused code

--- QMChatKit/QMChatKit/MessageViewComponent.mm
+++ QMChatKit/QMChatKit/MessageViewComponent.mm
@@ -19,58 +19,36 @@
     
     CKComponentScope scope(self);
     
-    MessageViewComponent *messageViewComponent =
+    CKComponentViewConfiguration stackMessageViewComponnentConfiguration = {
+        //CKComponentViewClass
+        [UIView class], {
+            //CKComponentViewAttributeValue
+            CKComponentTapGestureAttribute(@selector(didTap)), {
+                @selector(setBackgroundColor:), [UIColor whiteColor]
+            }
+        }
+    };
     
-    [super newWithComponent:
-     [CKStackLayoutComponent
-      newWithView:{
-          [UIView class],
-          {CKComponentTapGestureAttribute(@selector(didTap)),
-              {@selector(setBackgroundColor:), [UIColor whiteColor]}}
-          
-      }
-      size:{}
-      style:{
-          .alignItems = CKStackLayoutAlignItemsStretch
-      }
-      children:{
-          {
-              [QMIncomingMessageView newWithText:message.text context:context]
-          }
-      }
-      ]];
+    CKStackLayoutComponent *layoutComponent =
+    [CKStackLayoutComponent newWithView:stackMessageViewComponnentConfiguration
+                                   size:{}
+                                  style:{
+                                      .alignItems = CKStackLayoutAlignItemsStretch
+                                  }
+                               children:{
+                                   {
+                                       [QMIncomingMessageView newWithText:message.text context:context]
+                                   }
+                               }
+     ];
     
-    return messageViewComponent;
+    return [super newWithComponent:layoutComponent];
     
 }
 
 - (void)didTap {
     
     NSLog(@"Did Tap");
-}
-
-+ (id)initialState
-{
-    return @NO;
-}
-
-- (std::vector<CKComponentAnimation>)animationsFromPreviousComponent:(MessageViewComponent *)previousComponent
-{
-    if (previousComponent->_overlay == nil && _overlay != nil) {
-        return {{_overlay, scaleToAppear()}}; // Scale the overlay in when it appears.
-    }
-    else {
-        return {};
-    }
-}
-
-static CAAnimation *scaleToAppear()
-{
-    CABasicAnimation *scale = [CABasicAnimation animationWithKeyPath:@"transform"];
-    scale.fromValue = [NSValue valueWithCATransform3D:CATransform3DMakeScale(0.0, 0.0, 0.0)];
-    scale.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut];
-    scale.duration = 0.2;
-    return scale;
 }
 
 @end
